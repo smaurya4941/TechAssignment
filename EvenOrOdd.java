05e6f41a1e59b9f56194d7292293b0eea72649a7
sachin <smaurya4941@gmail.com>
2025-03-22 11:20:33 +0530

    odd commit

---
+++ EvenOrOdd.java
@@ -0,0 +1,10 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 29;
+
+  if (number % 2 == 0)
+           System.out.println(number + " is Even");
+  else
+           System.out.println(number + " is odd");
+   }
+}
