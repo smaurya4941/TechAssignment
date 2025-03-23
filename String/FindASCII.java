a0dac3525c3a99388579d996e6ba025a29ea3cb5
smaurya4941 <smaurya4941@gmail.com>
2025-03-23 21:44:10 +0530

    Find ASCII VAlue

---
+++ String/FindASCII.java
@@ -0,0 +1,9 @@
+package String;
+
+public class FindASCII {
+    public static void main(String[] args) {
+        char ch='d';
+        int ascii = ch;
+        System.out.println("ASCII value of "+ch+" is "+ascii);
+    }
+}
