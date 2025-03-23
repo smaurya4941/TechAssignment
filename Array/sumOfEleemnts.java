4fbaee2a881726681259353db40bead08997a45d
smaurya4941 <smaurya4941@gmail.com>
2025-03-23 18:46:44 +0530

    sum of elements

---
+++ Array/sumOfEleemnts.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class sumOfEleemnts {
+    public static void main(String[] args) {
+        int arr[] = {12, 13, 1, 10, 34, 10};
+        int sum=0;
+        for(int e:arr)sum+=e;
+        System.out.println("Sum of Elemnts : "+sum);
+    }
+}
