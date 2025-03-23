7f7cb6677dc1f4b42eb00f1bed068980288ace6c
smaurya4941 <smaurya4941@gmail.com>
2025-03-23 15:17:35 +0530

    Prime in Range

--- PrimeInRange.java
+++ PrimeInRange.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class PrimeInRange{
     public static void main(String[] args) {
+        ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+        Scanner sc =new Scanner(System.in);
         System.out.println("Jai Shree RadhKrishna");
+        System.out.println("Enter range:");
+        int lowerlimit=sc.nextInt();
+        int upperlimit=sc.nextInt();
+        if(lowerlimit<=2 || upperlimit<=2){
+            System.out.println("Enter range greater than 2");
+            return;
+        }
+        for(int i=lowerlimit;i<=upperlimit;i++){
+            if(Prime(i)){
+                primeNumbers.add(i);
+
+            }
+        }
+        System.out.println(primeNumbers);
+
+    }
+
+    public static  boolean Prime(int n){
+        int i=2;
+        boolean check=true;
+        while(i<=n/2){
+            if(n%i==0){
+                check=false;
+                break;
+            }
+            i++;
+        }
+        return check;
     }
 }
